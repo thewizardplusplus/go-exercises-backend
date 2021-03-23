46283c8aaba54fdb9c3ac3c7a8c10a10c770cec4
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-23 23:00:00 +0300

    Add the storages.OpenDB() function

---
+++ gateways/storages/open_db.go
@@ -0,0 +1,29 @@
+package storages
+
+import (
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/go-exercises-backend/entities"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// OpenDB ...
+func OpenDB(dbDSN string, logWriter logger.Writer) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(dbDSN), &gorm.Config{
+		Logger: logger.New(logWriter, logger.Config{
+			SlowThreshold: 0, // disallow analysis of query execution speed
+			Colorful:      false,
+			LogLevel:      logger.Info,
+		}),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to open the DB")
+	}
+
+	if err := db.AutoMigrate(&entities.Task{}); err != nil {
+		return nil, errors.Wrap(err, "unable to migrate the entities automatically")
+	}
+
+	return db, nil
+}
