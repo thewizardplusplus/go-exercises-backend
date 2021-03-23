fc210851c72ec2f4ccd61c47619679e959018277
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-23 23:00:00 +0300

    Implement the handlers.TaskHandler.UpdateTask() method

--- gateways/handlers/task_handler.go
+++ gateways/handlers/task_handler.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-exercises-backend/entities"
 	"gorm.io/gorm"
@@ -68,6 +70,29 @@ func (handler TaskHandler) UpdateTask(
 	writer http.ResponseWriter,
 	request *http.Request,
 ) {
+	idAsStr := mux.Vars(request)["id"]
+	id, err := strconv.ParseUint(idAsStr, 10, 64)
+	if err != nil {
+		err = errors.Wrap(err, "unable to decode the task ID")
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	var task entities.Task
+	if err := json.NewDecoder(request.Body).Decode(&task); err != nil {
+		err = errors.Wrap(err, "unable to decode the request body")
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	if err := handler.TaskStorage.UpdateTask(uint(id), task); err != nil {
+		err = errors.Wrap(err, "unable to update a task")
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
 }
 
 // DeleteTask ...
