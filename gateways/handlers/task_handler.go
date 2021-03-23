3ff950d968f8b3e15b20b77ae7d5348cfa29f6ea
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-23 23:00:00 +0300

    Add basics of the handlers.TaskHandler structure

--- gateways/handlers/task_handler.go
+++ gateways/handlers/task_handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/thewizardplusplus/go-exercises-backend/entities"
 )
 
@@ -11,4 +13,44 @@ type TaskStorage interface {
 	CreateTask(task entities.Task) (id uint, err error)
 	UpdateTask(id uint, task entities.Task) error
 	DeleteTask(id uint) error
+}
+
+// TaskHandler ...
+type TaskHandler struct {
+	TaskStorage TaskStorage
+}
+
+// GetTasks ...
+func (handler TaskHandler) GetTasks(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+}
+
+// GetTask ...
+func (handler TaskHandler) GetTask(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+}
+
+// CreateTask ...
+func (handler TaskHandler) CreateTask(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+}
+
+// UpdateTask ...
+func (handler TaskHandler) UpdateTask(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+}
+
+// DeleteTask ...
+func (handler TaskHandler) DeleteTask(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
 }
