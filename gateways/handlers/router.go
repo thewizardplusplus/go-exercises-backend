6c79803b3fd47accc7db26f2f9924d1ee8f14ea2
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-23 23:00:00 +0300

    Add the handlers.NewRouter() function

---
+++ gateways/handlers/router.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// RouterDependencies ...
+type RouterDependencies struct {
+	TaskStorage TaskStorage
+}
+
+// NewRouter ...
+func NewRouter(dependencies RouterDependencies) *mux.Router {
+	rootRouter := mux.NewRouter()
+	apiRouter := rootRouter.PathPrefix("/api").Subrouter()
+	apiV1Router := apiRouter.PathPrefix("/v1").Subrouter()
+
+	taskHandler := TaskHandler{TaskStorage: dependencies.TaskStorage}
+	tasksAPIV1Router := apiV1Router.PathPrefix("/tasks").Subrouter()
+	tasksAPIV1Router.
+		HandleFunc("/{id}", taskHandler.GetTask).
+		Methods(http.MethodGet)
+	tasksAPIV1Router.
+		HandleFunc("/{id}", taskHandler.UpdateTask).
+		Methods(http.MethodPut)
+	tasksAPIV1Router.
+		HandleFunc("/{id}", taskHandler.DeleteTask).
+		Methods(http.MethodDelete)
+	tasksAPIV1Router.
+		HandleFunc("/", taskHandler.GetTasks).
+		Methods(http.MethodGet)
+	tasksAPIV1Router.
+		HandleFunc("/", taskHandler.CreateTask).
+		Methods(http.MethodPost)
+
+	return rootRouter
+}
