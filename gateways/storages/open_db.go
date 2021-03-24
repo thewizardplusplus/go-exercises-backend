7e7c9bfc2b1376a6760748804c8f9b601fe1f0c4
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-24 23:00:00 +0300

    Use the gorm.io/driver/postgres package in the storages.OpenDB() function

--- gateways/storages/open_db.go
+++ gateways/storages/open_db.go
@@ -3,14 +3,14 @@ package storages
 import (
 	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-exercises-backend/entities"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 // OpenDB ...
 func OpenDB(dbDSN string, logWriter logger.Writer) (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open(dbDSN), &gorm.Config{
+	db, err := gorm.Open(postgres.Open(dbDSN), &gorm.Config{
 		Logger: logger.New(logWriter, logger.Config{
 			SlowThreshold: 0, // disallow analysis of query execution speed
 			Colorful:      false,
