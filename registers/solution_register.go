926b4ba44b9b5a6cffe16d8805cba5062159e355
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-25 23:00:00 +0300

    Add the registers.SolutionRegister structure

---
+++ registers/solution_register.go
@@ -0,0 +1,52 @@
+package registers
+
+import (
+	"github.com/go-log/log"
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/go-exercises-backend/entities"
+	"github.com/thewizardplusplus/go-exercises-backend/gateways/handlers"
+)
+
+// SolutionQueue ...
+type SolutionQueue interface {
+	AddSolution(solution entities.Solution) error
+}
+
+// SolutionRegister ...
+type SolutionRegister struct {
+	TaskStorage     handlers.TaskStorage
+	SolutionStorage handlers.SolutionStorage
+	SolutionQueue   SolutionQueue
+	Logger          log.Logger
+}
+
+// RegisterSolution ...
+func (register SolutionRegister) RegisterSolution(id uint) {
+	if err := register.performRegistration(id); err != nil {
+		err = errors.Wrapf(err, "[error] unable to register solution #%d", id)
+		register.Logger.Log(err)
+
+		return
+	}
+
+	register.Logger.Logf("[info] solution #%d has been registered", id)
+}
+
+func (register SolutionRegister) performRegistration(id uint) error {
+	solution, err := register.SolutionStorage.GetSolution(id)
+	if err != nil {
+		return errors.Wrap(err, "unable to get the solution")
+	}
+
+	task, err := register.TaskStorage.GetTask(solution.TaskID)
+	if err != nil {
+		return errors.Wrap(err, "unable to get the task")
+	}
+
+	solution.Task = task
+	if err := register.SolutionQueue.AddSolution(solution); err != nil {
+		return errors.Wrap(err, "unable to enqueue the solution")
+	}
+
+	return nil
+}
