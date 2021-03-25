82cb2a1d0dc1c4008cfa0c6ef9e3ae954a5174e9
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-25 23:00:00 +0300

    Add the queues.SolutionQueue structure

---
+++ gateways/queues/solution_queue.go
@@ -0,0 +1,42 @@
+package queues
+
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+	"github.com/streadway/amqp"
+	"github.com/thewizardplusplus/go-exercises-backend/entities"
+)
+
+// SolutionQueue ...
+type SolutionQueue struct {
+	client Client
+}
+
+// NewSolutionQueue ...
+func NewSolutionQueue(client Client) SolutionQueue {
+	return SolutionQueue{client: client}
+}
+
+// AddSolution ...
+func (queue SolutionQueue) AddSolution(solution entities.Solution) error {
+	solutionAsJSON, err := json.Marshal(solution)
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal the solution")
+	}
+
+	if err := queue.client.channel.Publish(
+		"",               // exchange
+		"solution_queue", // queue name
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        solutionAsJSON,
+		},
+	); err != nil {
+		return errors.Wrap(err, "unable to publish the solution")
+	}
+
+	return nil
+}
