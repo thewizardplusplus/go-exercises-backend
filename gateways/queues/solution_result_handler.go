9376c7a2ba1acf75089f11bead5c96922d5d09cd
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-26 23:00:00 +0300

    Improve the error handling in the queues.SolutionResultHandler structure

--- gateways/queues/solution_result_handler.go
+++ gateways/queues/solution_result_handler.go
@@ -24,32 +24,37 @@ type SolutionResultHandler struct {
 func (handler SolutionResultHandler) HandleSolutionResult(
 	message amqp.Delivery,
 ) {
-	if err := handler.performHandling(message); err != nil {
-		err = errors.Wrap(err, "[error] unable to handle the solution result")
-		handler.Logger.Log(err)
+	solution, err := handler.performHandling(message)
+	if err != nil {
+		handler.Logger.Log(errors.Wrapf(
+			err,
+			"[error] unable to handle the result of solution #%d",
+			solution.ID,
+		))
 
 		message.Reject(true)
 		return
 	}
 
-	handler.Logger.Log("[info] solution result has been handled")
+	handler.Logger.
+		Logf("[info] result of solution #%d has been handled", solution.ID)
 	message.Ack(false)
 }
 
 // HandleSolutionResult ...
 func (handler SolutionResultHandler) performHandling(
 	message amqp.Delivery,
-) error {
+) (entities.Solution, error) {
 	var solution entities.Solution
 	if err := json.Unmarshal(message.Body, &solution); err != nil {
-		return errors.Wrap(err, "unable to unmarshal the solution")
+		return solution, errors.Wrap(err, "unable to unmarshal the solution")
 	}
 
 	if err := handler.SolutionResultRegister.RegisterSolutionResult(
 		solution,
 	); err != nil {
-		return errors.Wrap(err, "unable to register the solution result")
+		return solution, errors.Wrap(err, "unable to register the solution result")
 	}
 
-	return nil
+	return solution, nil
 }
