2861c86c84020e0f8e9f2dfaa4b6277fc230ba20
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-30 23:00:00 +0300

    Add the storages.UserStorage structure

---
+++ gateways/storages/user_storage.go
@@ -0,0 +1,45 @@
+package storages
+
+import (
+	"github.com/thewizardplusplus/go-exercises-backend/entities"
+	"gorm.io/gorm"
+)
+
+// UserStorage ...
+type UserStorage struct {
+	db          *gorm.DB
+	hashingCost int
+}
+
+// NewUserStorage ...
+func NewUserStorage(db *gorm.DB, hashingCost int) UserStorage {
+	return UserStorage{db: db, hashingCost: hashingCost}
+}
+
+// GetUser ...
+func (storage UserStorage) GetUser(username string) (entities.User, error) {
+	var user entities.User
+	err := storage.db.
+		Where(&entities.User{Username: username}).
+		First(&user).
+		Error
+	if err != nil {
+		return entities.User{}, err
+	}
+
+	return user, nil
+}
+
+// CreateUser ...
+func (storage UserStorage) CreateUser(user entities.User) error {
+	user.Model = gorm.Model{} // reset the fields that are filled in automatically
+	if err := user.HashPassword(storage.hashingCost); err != nil {
+		return err
+	}
+
+	if err := storage.db.Create(&user).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
