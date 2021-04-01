4c994970bb95afbe5e5b73afef9b392ef1c19c0c
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-04-01 23:00:00 +0300

    Complete the handlers.NewRouter() function

--- gateways/handlers/routing.go
+++ gateways/handlers/routing.go
@@ -2,22 +2,33 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-log/log"
 	"github.com/gorilla/mux"
 	"github.com/thewizardplusplus/go-exercises-backend/entities"
 )
 
+// RouterOptions ...
+type RouterOptions struct {
+	TokenSigningKey string
+	TokenTTL        time.Duration
+}
+
 // RouterDependencies ...
 type RouterDependencies struct {
 	TaskStorage      TaskStorage
 	SolutionStorage  SolutionStorage
 	SolutionRegister entities.SolutionRegister
+	UserGetter       UserGetter
 	Logger           log.Logger
 }
 
 // NewRouter ...
-func NewRouter(dependencies RouterDependencies) *mux.Router {
+func NewRouter(
+	options RouterOptions,
+	dependencies RouterDependencies,
+) *mux.Router {
 	rootRouter := mux.NewRouter()
 	apiRouter := rootRouter.PathPrefix("/api/v1").Subrouter()
 
@@ -55,6 +66,16 @@ func NewRouter(dependencies RouterDependencies) *mux.Router {
 	apiRouter.
 		HandleFunc("/solutions/{id}", solutionHandler.GetSolution).
 		Methods(http.MethodGet)
+
+	tokenHandler := TokenHandler{
+		TokenSigningKey: options.TokenSigningKey,
+		TokenTTL:        options.TokenTTL,
+		UserGetter:      dependencies.UserGetter,
+		Logger:          dependencies.Logger,
+	}
+	apiRouter.
+		HandleFunc("/tokens/", tokenHandler.CreateToken).
+		Methods(http.MethodPost)
 
 	return rootRouter
 }
