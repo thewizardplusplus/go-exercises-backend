5cedb647290378a6213ce5d9c1c06754abc093a2
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-08 23:00:00 +0300

    Add the task_status_with_task.sql script

---
+++ docs/sql/task_status_with_task.sql
@@ -0,0 +1,12 @@
+SELECT tasks.*, statuses.status
+FROM tasks
+JOIN (
+  SELECT tasks.id AS tasks_id, MAX(CASE
+    WHEN is_correct THEN 2
+    WHEN NOT is_correct AND result != '{}' THEN 1
+    ELSE 0
+  END) AS status
+  FROM tasks
+  LEFT JOIN solutions ON solutions.task_id = tasks.id
+  GROUP BY tasks.id
+) statuses ON statuses.tasks_id = tasks.id
